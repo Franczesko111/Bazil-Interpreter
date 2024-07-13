97489813370e7d49219c859af6ace21c79811b06
Franczesko111 <franczeskostr@gmail.com>
2024-07-13 12:22:37 +0100

    Loading file content
    Now you can load the main.bz file and read code through it (for now only "main.bz" is available!)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+#include <fstream>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::ifstream file("main.bz");
+    std::string data;
+    std::vector<std::string> tokens;
+    while(file.eof() == false)
+    {
+        getline(file, data);
+        std::cout << data << std::endl;
+    }
     return 0;
 }
